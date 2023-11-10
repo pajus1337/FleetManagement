e25d5835f79fe2452e3c8b279844220eefce5399
pajusco <pajus1337@gmail.com>
2023-11-10 14:46:07 +0100

    Project update : - Created TheConcep&devLog File - Created MenuAction class - Defined to-do list for "stage 1" - The current version of the application has been defined

--- FleetManagement/Program.cs
+++ FleetManagement/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string version = "v0.1a";
+
+            /* TODO :
+             * Create a simple menu based on a few functions: 
+             * 1) Adding a vehicle (creating it)
+             * 2) Deleting a vehicle
+             * 3) Displaying the fleet status
+             * 4) Return vehicle lists based on a predefined filter
+             * 1a) Selecting the vehicle type
+             * 1b) Adding vehicle details
+             * 2a) Entering the ID or registration plate of the vehicle
+             * 2b) Vehicle will be deleted
+             * 3a) Querying the vehicle ID 
+             * 3b) Displaying all information about the vehicle
+             * 4a) Entering the vehicle type
+             * 5a) Calling up a list of vehicles of a given type
+            */
+
+            Console.WriteLine($"Welcome to Fleet Management {version}");
+
+
         }
     }
 }
